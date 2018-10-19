05ec58423269fe8df9920e83411cafe35ede5cc9
DESKTOP-N1D5NOF\User <kimdaesoop@gmail.com>
2018-10-19 17:51:11 +0900

    ladder step5

--- src/main/java/view/PrintView.java
+++ src/main/java/view/PrintView.java
@@ -3,32 +3,42 @@ package view;
 import ladder.Ladder;
 import ladder.Line;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class PrintView {
     static StringBuilder stringBuilder = new StringBuilder();
 
-    public static void totalPrint(Ladder ladder, String[] people) {
+    public static void totalPrint(Ladder ladder, String[] result) {
+        String showResult = InputView.showResult();
         List<Line> ladders = ladder.getLadders();
-        printResult(ladders, people);
+        String[] names = ladder.getPeople();
+        HashMap<Integer, Integer> resultHash = ladder.moveLine();
+        printResult(ladders, names, result);
+        checkResult(resultHash, result, names, showResult);
         System.out.println(String.valueOf(stringBuilder));
     }
 
-    public static void printResult(List<Line> ladders, String[] people) {
-        for (int i = 0; i < people.length; i++) {
-            stringBuilder.append(String.format("%6s", people[i]));
+    public static void printResult(List<Line> ladders, String[] names, String[] result) {
+        for (int i = 0; i < names.length; i++) {
+            stringBuilder.append(String.format("%-6s", names[i]));
         }
         stringBuilder.append("\n");
         for (int i = 0; i < ladders.size(); i++) {
-            insidePrintResult(ladders, people, i);
+            insidePrintResult(ladders, names, i);
             stringBuilder.append("\n");
         }
+
+        for (int i = 0; i < result.length; i++) {
+            stringBuilder.append(String.format("%-6s", result[i]));
+        }
+        stringBuilder.append("\n");
     }
 
-    private static void insidePrintResult(List<Line> ladders, String[] people, int i) {
-        for (int j = 0; j < people.length; j++) {
-            stringBuilder.append(printDash(ladders, i, j));
+    private static void insidePrintResult(List<Line> ladders, String[] names, int i) {
+        for (int j = 0; j < names.length; j++) {
             printBar();
+            stringBuilder.append(printDash(ladders, i, j));
         }
     }
 
@@ -41,5 +51,19 @@ public class PrintView {
             return "-----";
         }
         return "     ";
+    }
+
+    public static void checkResult(HashMap<Integer, Integer> hashMap, String[] result, String[] names, String resultShow) {
+        if (resultShow.equals("all")) {
+            for (int i = 0; i < result.length; i++) {
+                stringBuilder.append(names[i] + " : " + result[hashMap.get(i)]);
+                stringBuilder.append("\n");
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            if (resultShow.equals(names[i])) {
+                stringBuilder.append(result[hashMap.get(i)]);
+            }
+        }
     }
 }
